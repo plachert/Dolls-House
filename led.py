b4085551c3eb4dc8bdaad03636e5ae1a0b5dfff9
plachert <plachert@wp.pl>
2018-06-21 20:14:02 +0000

    properties

--- led.py
+++ led.py
@@ -7,7 +7,25 @@ class Pin:
         GPIO.setwarnings(False)
         self.number = number
         self.state = False
+    
+    @property
+    def number(self):
+        return self._number
+    
+    @number.setter
+    def number(self, number):
+        if number in range(1, 27):
+            self._number = number
+        else:
+            raise ValueError
 
 class Led(Pin):
-
-    pass
+    
+    PINS = [5,6,16,17,22,23]
+    
+    @Pin.number.setter
+    def number(self, number):
+        if number in Led.PINS:
+            self._number = number
+        else:
+            raise ValueError
