fbe922caa6f375be9eb97b2b8e2b1015d85aec5a
plachert <plachert@wp.pl>
2018-09-25 20:29:08 +0000

    button

--- GUI.py
+++ GUI.py
@@ -3,8 +3,20 @@ import tkinter as tk
 
 
 class RpiGUI(tk.Frame):
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self*args, **kwargs)
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+
+class PinButton(tk.Button):
+    def __init__(self, parent, pin, *args, **kwargs):
+        tk.Button.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
+        self.pin = pin
+    
+if __name__ == '__main__':
+    root = tk.Tk()
+    RpiGUI(root)
+    root.mainloop()
 #GPIO.setmode(GPIO.BCM)
 #channel = 5
 #GPIO.setup(channel, GPIO.OUT)
