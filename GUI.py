3f092768f67919095c27329b2e5633b832925b1d
plachert <plachert@wp.pl>
2018-09-25 20:09:25 +0000

    gui

--- GUI.py
+++ GUI.py
@@ -1,5 +1,11 @@
-from led import Led
-from threading import Thread
+import RPi.GPIO as GPIO
+import tkinter as tk
 
-class GUI(object):
-    pass
+
+class RpiGUI(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self*args, **kwargs)
+#GPIO.setmode(GPIO.BCM)
+#channel = 5
+#GPIO.setup(channel, GPIO.OUT)
+#print(GPIO.input(channel))
